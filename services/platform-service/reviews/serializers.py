8bb7f18174f972ea360e86822f68502c2de67b88
LeonStansfield <leonstansfield208@gmail.com>
2026-02-19 12:46:35 +0000

    updated frontend to show basic info on new DB

--- services/platform-service/reviews/serializers.py
+++ services/platform-service/reviews/serializers.py
@@ -3,8 +3,9 @@ from .models import Review
 
 class ReviewSerializer(serializers.ModelSerializer):
     customer_username = serializers.ReadOnlyField(source='customer.username')
+    product_name = serializers.ReadOnlyField(source='product.name')
 
     class Meta:
         model = Review
-        fields = ('id', 'customer', 'customer_username', 'product', 'order', 'rating', 'comment', 'created_at')
+        fields = ('id', 'customer', 'customer_username', 'product', 'product_name', 'order', 'rating', 'comment', 'created_at')
         read_only_fields = ('id', 'customer', 'created_at')
