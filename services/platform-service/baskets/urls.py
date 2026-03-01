45dcad03b0dc9f0ee34228b840a01cf1c51722c2
Dina Metwalli <dina.moh657@gmail.com>
2026-03-01 12:36:28 +0000

    feat(basket): add url patterns

---
+++ services/platform-service/baskets/urls.py
@@ -0,0 +1,17 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from .views import (
+    BasketView,
+    AddToBasketView,
+    UpdateBasketItemView,
+    RemoveFromBasketView,
+    ClearBasketView
+)
+
+urlpatterns = [
+    path('', BasketView.as_view(), name='basket-detail'),
+    path('add/', AddToBasketView.as_view(), name='basket-add'),
+    path('items/<int:item_id>/', UpdateBasketItemView.as_view(), name='basket-item-update'),
+    path('items/<int:item_id>/remove/', RemoveFromBasketView.as_view(), name='basket-item-remove'),
+    path('clear/', ClearBasketView.as_view(), name='basket-clear'),
+]
